ca4de2f47a55386dd83b59f053144201b85c483f
techsolutionss <aulegabriel381@gmail.com>
2024-12-19 15:04:07 +0100

    first commit

---
+++ config/settings/local.py
@@ -0,0 +1,23 @@
+from .base import *
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="django-insecure-g(bfc=+_rty%@0&$c7%du_gs@k2o!m1+i09chxru1_+g^fpw8&",
+)
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+# CACHES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
+    }
+}
